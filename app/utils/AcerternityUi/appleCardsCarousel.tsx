e027169de5d5af8f6940cb34016aff19f85edb0a
alexis142mayer <57840288+alexis142mayer@users.noreply.github.com>
2025-11-09 09:47:41 -0500

    adjusted carousel animation

--- app/utils/AcerternityUi/appleCardsCarousel.tsx
+++ app/utils/AcerternityUi/appleCardsCarousel.tsx
@@ -110,11 +110,11 @@ export const Carousel = ({ items, initialScroll = 0 }: CarouselProps) => {
                   opacity: 0,
                   y: 20,
                 }}
-                animate={{
+                whileInView={{
                   opacity: 1,
                   y: 0,
                   transition: {
-                    duration: 0.5,
+                    duration: 0.8,
                     delay: 0.2 * index,
                     ease: "easeOut",
                   },
